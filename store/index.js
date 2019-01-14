d9fb8b1b866a1774c24f69e0bb5af4611cf405cd
Dennis <denniskamau3@gmail.com>
2019-01-14 19:11:22 +0300

    Add navbar add form data binding

--- store/index.js
+++ store/index.js
@@ -1,11 +1,1 @@
-export const getters = {
-  // return the authentication status
-  isAuthenticated(state) {
-    return state.auth.loggedIn
-  },
-
-  // return the details of the loggein user
-  loggedInUser(state) {
-    return state.auth.user
-  }
-}
+import auth from './modules/auth';
