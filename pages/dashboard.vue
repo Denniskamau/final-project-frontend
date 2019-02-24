7be3419ea8ae16c5ccd7e587520b697433c1ce10
Dennis <denniskamau3@gmail.com>
2019-02-24 22:55:35 +0300

    search working

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,13 +1,66 @@
 <template>
-  <div>
-    <h1>Hallo from dashboard</h1>
-  </div>
+  <section class="section">
+    <div class="container">
+      <div class="column is-4 is-offset-4">
+      <form method="post" @submit.prevent="startStream">
+      <p>Input search term to start analysis</p>
+      <div class="field">
+        <input class="input is-rounded" type="text"
+        placeholder="Input text"
+        name="search"
+        v-model="search.query"
+        >
+      </div>
+      <div class="field is-grouped">
+        <div class="control">
+            <button class="button is-link is-rounded">Search</button>
+        </div>
+    </div>
+      </form>
+      </div>
+      <div>
+        <table class="table is-bordered is-striped is-narrow is-hoverable" v-if="finish">
+          <thead>
+            <tr>
+              <th>Positive text</th>
+              <th>Negative text</th>
+            </tr>
+          </thead>
+          <tbody v-for="res in results" :key="res">
+            <td v-if="res.message === 'Positive Sentiment'">{{res.tweet}}</td>
+            <td v-if="res.message === 'Negative Sentiment'">{{res.tweet}}</td>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  middleware: 'strict'
-
+  middleware: 'strict',
+  name: 'dashboard',
+  data () {
+    return {
+      search: {},
+      results: {},
+      finish:false
+    }
+  },
+  methods: {
+    // start streaming data
+    async startStream() {
+      try {
+        let resp = await axios.post('http://127.0.0.1:5000/stream', this.search)
+        this.results = resp.data
+        this.finish = true
+        console.log(`response ${JSON.stringify(this.results)}`)
+      }catch (e){
+        return e
+      }
+    }
+  }
 
 }
 </script>
